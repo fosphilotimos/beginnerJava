1e78fc297b5553dd27ec455669deadec90606f92
Bawara <rydhan.bw@gmail.com>
2020-10-06 17:22:21 +0700

    add while on script

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello");
+
+        //print DooBeeDooBeeDo
+        int x = 1;
+
+        //Looping DooBee
+        while (x < 3) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+
+            x = x + 1;
+        }
+
+        if (x == 3) {
+            System.out.println("Do");
+        }
     }
 }
